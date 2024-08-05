a57a178b9d6b05c7025861376750c1d48a11be63
Igor <igor@zavirukhin.com>
2024-08-05 03:51:12 +0300

    feat: add ripple

--- js/index.js
+++ js/index.js
@@ -1,8 +1,41 @@
 const editableElements = document.getElementsByClassName("editable");
+const matRippleElement = document.getElementsByClassName("mat-ripple");
 
 function eventHandler(event) {
   const target = event.target;
   localStorage.setItem(this.context, target.innerText);
+}
+
+function createMatRipplePoint(targetElement, x, y) {
+  const boundry = targetElement.getBoundingClientRect();
+  const sizePoint = Math.max(boundry.width, boundry.height) / 5;
+  const matPoint = document.createElement("div");
+  
+  matPoint.classList.add("mat-ripple-point");
+  matPoint.style.top = y + "px";
+  matPoint.style.left = x + "px";
+  matPoint.style.width = sizePoint + "px";
+  matPoint.style.height = sizePoint + "px";
+
+  targetElement.appendChild(matPoint);
+  matPoint.addEventListener("animationend", (e) => targetElement.remove());
+}
+
+function showMatRipple(clickEvent) {
+  console.log(clickEvent);
+  const x = clickEvent.layerX;
+  const y = clickEvent.layerY;
+
+  const element = document.createElement("DIV");
+  element.classList.add("mat-ripple-view");
+  this.context.appendChild(element);
+
+  createMatRipplePoint(element, x, y);
+}
+
+for (const element of matRippleElement) {
+  const handler = showMatRipple.bind({ context: element });
+  element.addEventListener("click", handler);
 }
 
 for (let i = 0; i < editableElements.length; i++) {
